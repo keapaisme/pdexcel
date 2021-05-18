4b5a0a31a0eb5e9f0d4dbea8566e3a5dfb37db50
DESKTOP-7OPJ51K\kas <kaskaskas>
2021-05-19 03:01:54 +0800

    matplotlib bar chart

--- 009.py
+++ 009.py
@@ -1,4 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
-students = pd.read_excel('temp/Students.xlsx', index_col=0)
+
+font = FontProperties(fname=r"C:\Windows\Fonts\STHUPO.TTF", size=14)
+students = pd.read_excel('temp/Students.xlsx')
+# print(students)
+students.sort_values(by='score', inplace=True, ascending=False)
+# students.plot.bar(x='student', y='score', color='red', title='2nd Exam Score')
+plt.bar(students.student, students.score, width=0.5)
+plt.xticks(students.student, rotation='90')
+
+plt.title('期中考成绩', FontProperties=font)
+plt.xlabel('姓名', FontProperties=font)
+plt.ylabel('分数', FontProperties=font)
+plt.tight_layout()
+plt.show()
